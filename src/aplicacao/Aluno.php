848684967bffd08f3616c76bd750e963848364de
Raffael Guideti Miello <raffaelguideti@gmail.com>
2024-06-07 10:01:30 -0300

    Correção de metodo post
    Require ainda da erro (pensando em talvez trocar por include por recomendação)

--- src/aplicacao/Aluno.php
+++ src/aplicacao/Aluno.php
@@ -2,21 +2,21 @@
 
 class Aluno
 {
-    private string $Ra;
+    private string $ra;
     private string $nome;
     private string $email;
     
 
-    public function __construct(string $Ra, string $nome, string $email)
+    public function __construct(string $ra, string $nome, string $email)
     {
         $this->nome = $nome;
-        $this->Ra = $Ra;
+        $this->ra = $ra;
         $this->email = $email;
     }
 
-    public function getRa(): string
+    public function getra(): string
     {
-        return $this->Ra;
+        return $this->ra;
     }
 
     public function getNome(): string
--- src/app.php
+++ src/app.php
@@ -1,11 +1,12 @@
 <?php
 
-require_once __DIR__ . '/src/database.php';
+require_once __DIR__. 'aplicacao\database.php';
+require_once __DIR__. 'aplicacao\Aluno.php';
+
+$pdo = database::getConn();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $ra = $_POST['Ra'];
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
+    $Aluno = new Aluno($_POST['nome'], $_POST['ra'], $_POST['email']);
 
     $stmt = $pdo->prepare('INSERT INTO Alunos (ra, nome, email) VALUES (:ra, :nome, :email)');
     $stmt->bindParam(':ra', $ra);
