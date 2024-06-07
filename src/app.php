e7de2674bc5629bac7abc4098f27e2e470b4dcd6
Raffael Guideti Miello <raffaelguideti@gmail.com>
2024-06-07 00:31:56 -0300

    Tentativa de integrar banco
    Require acusa erro

--- src/app.php
+++ src/app.php
@@ -1,3 +1,24 @@
 <?php
 
 require_once __DIR__ . '/src/database.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $ra = $_POST['Ra'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+
+    $stmt = $pdo->prepare('INSERT INTO Alunos (ra, nome, email) VALUES (:ra, :nome, :email)');
+    $stmt->bindParam(':ra', $ra);
+    $stmt->bindParam(':nome', $nome);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+
+    header('Location: sucesso.html');
+    exit();
+}
+
+else{
+    echo "impossivel conectar no banco";
+}
+
+$pdo = null;
