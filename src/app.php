b33247ea7f17bb02292353fc4ccc826a14bdce79
Rafael Osadzuk <rafael.osadzuk@gmail.com>
2024-06-08 15:51:26 -0300

    criando aluno model e tentando salvar os dados no banco

--- src/app.php
+++ src/app.php
@@ -1,25 +1,33 @@
 <?php
 
-require_once __DIR__. 'aplicacao\database.php';
-require_once __DIR__. 'aplicacao\Aluno.php';
+require_once __DIR__ . '/aplicacao/database.php';
+require_once __DIR__ . '/aplicacao/Aluno.php';
+require_once __DIR__ . '/aplicacao/AlunoModel.php';
 
-$pdo = database::getConn();
+$pdo = Database::getConn();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $Aluno = new Aluno($_POST['nome'], $_POST['ra'], $_POST['email']);
-
-    $stmt = $pdo->prepare('INSERT INTO Alunos (ra, nome, email) VALUES (:ra, :nome, :email)');
-    $stmt->bindParam(':ra', $ra);
-    $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-
-    header('Location: sucesso.html');
+ 
+    $aluno = new Aluno($_POST['nome'], $_POST['Ra'], $_POST['email']);
+    
+   
+    $alunoModel = new AlunoModel($aluno);
+    if ($alunoModel->save()) {
+        
+        header('Location: sucesso.html');
+        exit();
+    } else {
+        
+        echo "Não foi possível salvar o aluno no banco de dados.";
+    }
+} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $stmt = $pdo->query('SELECT Ra, nome, email FROM Alunos');
+    $alunos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    header('Content-Type: application/json');
+    echo json_encode($alunos);
     exit();
+} else {
+    echo "Impossível conectar no banco";
 }
-
-else{
-    echo "impossivel conectar no banco";
-}
-
-$pdo = null;
