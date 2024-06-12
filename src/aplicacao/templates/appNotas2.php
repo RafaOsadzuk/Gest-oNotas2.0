4356cec59a06ef4652b444b7f02f3d1578f1b339
Raffael Guideti Miello <raffaelguideti@gmail.com>
2024-06-12 09:49:19 -0300

    Adição de fomulários e PHP

---
+++ src/aplicacao/templates/appNotas2.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/aplicacao/database.php';
+require_once __DIR__ . '/aplicacao/Aluno.php';
+require_once __DIR__ . '/aplicacao/AlunoModel.php';
+require_once __DIR__ . '/aplicacao/Notas.php';
+require_once __DIR__ . '/aplicacao/NotasModel.php';
+
+$pdo = Database::getConn();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $stmt = $pdo->query('SELECT Ra, nome, email FROM Alunos');
+    $alunos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ 
+    $notas = new Notas($_POST['prova2'], $_POST['aep2'], $_POST['prova_int2']);
+    
+   
+    $notasModel = new NotasModel($notas);
+    if ($notasModel->save()) {
+        
+        header('Location: sucesso.html');
+        exit();
+    } else {
+        
+        echo "Não foi possível salvar o aluno no banco de dados.";
+    }
+}
+ 
+else {
+    echo "Impossível conectar no banco";
+}
